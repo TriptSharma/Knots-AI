f600965d41ecaacd4593a1ecd6691d813096b4e1
vipulgarg1998 <garg.vipul7@gmail.com>
2020-02-22 04:10:41 +0530

    real_time_database_listener_update

--- demo.py
+++ demo.py
@@ -12,25 +12,38 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 storage = firebase.storage()
 available = 1
-
-print 1
+query_key = None
 run = 0
 data_path = None
+
 def stream_handler(message):
 	global run
-	global available
+	global query_key
 	data = message["data"]
+	keys = data.keys()
 	if run == 0:
-		for i in data.values():
+		for [num,i] in enumerate(data.values()):
 			if i['pStatus']==0 and available == 1:
 				data_path = i['data']
-				print(data_path)
-				available == 0
-				break
+				query_key = keys[num]
+				print(query_key)
+				# available == 0
+				# break
 		run = 1
 	elif run ==1:
 		if data['pStatus']==0 and available == 1:
 				data_path = data['data']
-				print(data_path)
 				available == 0
+				query_key = message["path"][1:]
+				perform_task()
+
+def perform_task():
+	update_status(2)
+
+def update_status(status):
+	global query_key
+	db.child("CVP").child(query_key).update({"pStatus": status})
+	print status
+
+
 my_stream = db.child("CVP").stream(stream_handler)
